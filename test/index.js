3c8dca6bc96e301abdaa4a60bd8b15e97b7919d6
Jeffrey van Hoven <wolferine@live.se>
2015-05-14 16:27:25 +0200

    Added module

---
+++ test/index.js
@@ -0,0 +1,30 @@
+var assert = require("assert");
+var gitignore = require("../src/gitignore.js");
+
+describe("gitignore utility functions", function() {
+	
+	before(function() {
+		gitignore.test.init("test/.test-gitignore");
+	});
+	
+	describe("read()", function() {
+		it("should remove comments and whitespace from .test-gitignore and return an array", function(done) {
+			gitignore.read(function(err, data) {
+				if(err) throw(err);
+				assert.equal(11, data.length);
+				done();
+			});
+		});
+	});
+	
+	describe("sort()", function() {
+		it("should sort files and directories in two seperate arrays", function(done) {
+			gitignore.sort(function(directories, files) {
+				assert.equal(6, directories.length);
+				assert.equal(5, files.length);
+				done();
+			});
+		});
+	});
+	
+});
